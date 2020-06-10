9edf67559b8621ceb1dc9587f6c7d72c49a52575
Willehard Korander <willehard@gmail.com>
2020-06-10 17:04:19 +0300

    add mlx_image struct with functions

---
+++ src/mlx_image.c
@@ -0,0 +1,40 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   mlx_image.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: wkorande <willehard@gmail.com>             +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/06/10 16:00:45 by wkorande          #+#    #+#             */
+/*   Updated: 2020/06/10 16:02:19 by wkorande         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "rt.h"
+
+t_mlx_img	*create_mlx_image(t_mlx *mlx, int width, int height)
+{
+	t_mlx_img *img;
+
+	if (!mlx)
+		return (NULL);
+	if (!(img = (t_mlx_img*)malloc(sizeof(t_mlx_img))))
+		panic("create_mlx_image failed!");
+	img->width = width;
+	img->height = height;
+	if (!(img->img = mlx_new_image(mlx->mlx_ptr,
+									img->width, img->height)))
+		panic("mlx_new_image failed!");
+	if (!(img->d_addr = mlx_get_data_addr(img->img, &img->bpp,
+										&img->size_line, &img->endian)))
+		panic("mlx_get_data_addr failed!");
+	img->bpp /= 8;
+	return (img);
+}
+
+void		put_pixel_mlx_img(t_mlx_img *img, int x, int y, int c)
+{
+	if (x < 0 || x >= img->width || y < 0 || y >= img->height)
+		return ;
+	*(int*)(img->d_addr + (((y * img->width) + x) * img->bpp)) = c;
+}
