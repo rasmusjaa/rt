2ad704fc415ab4faf612bcd8a66de56bf14ca638
Willehard Korander <willehard@gmail.com>
2020-06-12 12:59:59 +0300

    add file for real

---
+++ src/rt.c
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   rt.c                                               :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: wkorande <willehard@gmail.com>             +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/06/12 12:21:14 by wkorande          #+#    #+#             */
+/*   Updated: 2020/06/12 12:35:46 by wkorande         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "rt.h"
+#include "thread_pool.h"
+
+t_rt	*rt_init(size_t num_scenes)
+{
+	t_rt	*rt;
+
+	if (!(rt = (t_rt*)malloc(sizeof(t_rt))))
+		exit_message("Failed to malloc rt!");
+	if (!(rt->scenes = (t_scene**)malloc(sizeof(t_scene*) * num_scenes)))
+		exit_message("Failed to malloc rt->scenes!");
+	rt->num_scenes = num_scenes;
+	rt->cur_scene = 0;
+	rt->tp_render = tp_create(N_THREADS);
+	return (rt);
+}
+
+void	rt_destroy_exit(t_rt *rt, int status)
+{
+	tp_destroy(rt->tp_render);
+	exit(status);
+}
