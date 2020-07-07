3cdacce7030eb45f7fc2197a87a3c7b6587dcdfc
Willehard Korander <willehard@gmail.com>
2020-07-07 18:58:53 +0300

    add test

--- octree_test.c
+++ octree_test.c
@@ -1,27 +1,19 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   octree.h                                           :+:      :+:    :+:   */
+/*   octree_test.c                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: wkorande <willehard@gmail.com>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2020/07/07 18:38:03 by wkorande          #+#    #+#             */
-/*   Updated: 2020/07/07 18:57:51 by wkorande         ###   ########.fr       */
+/*   Created: 2020/07/07 18:55:12 by wkorande          #+#    #+#             */
+/*   Updated: 2020/07/07 18:58:36 by wkorande         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef OCTREE_H
-# define OCTREE_H
+#include "octree.h"
 
-# define NUM_CHILDREN 8
-
-typedef struct	s_octree
+int main(void)
 {
-	t_octree	*children[NUM_CHILDREN];
-	void		*content;
-}				t_octree;
+	t_octree *o;
 
-
-t_octree	*octree_create_node(void *content);
-
-#endif
+}
