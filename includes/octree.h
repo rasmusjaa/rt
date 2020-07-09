01a72af787b91fe6c578ff05dfafa54037388b11
Willehard Korander <willehard@gmail.com>
2020-07-09 14:57:21 +0300

    tweaks

--- includes/octree.h
+++ includes/octree.h
@@ -6,7 +6,7 @@
 /*   By: wkorande <willehard@gmail.com>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/07/07 18:38:03 by wkorande          #+#    #+#             */
-/*   Updated: 2020/07/07 20:04:59 by wkorande         ###   ########.fr       */
+/*   Updated: 2020/07/09 14:31:06 by wkorande         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,16 @@
 
 # define NUM_CHILDREN 8
 
+# define TOP_LEFT_FRONT			1<<0
+# define TOP_RIGHT_FRONT		1<<1
+# define BOTTOM_LEFT_FRONT		1<<2
+# define BOTTOM_RIGHT_FRONT		1<<3
+# define TOP_LEFT_BACK			1<<4
+# define TOP_RIGHT_BACK			1<<5
+# define BOTTOM_LEFT_BACK		1<<6
+# define BOTTOM_RIGHT_BACK		1<<7
+# define ACTIVE_NONE			0
+
 typedef struct	s_octree_content
 {
 	void		*data;
@@ -26,8 +36,11 @@ typedef struct	s_octree_content
 
 typedef struct	s_octree
 {
-	struct s_octree	*children[NUM_CHILDREN];
+	struct s_octree		*parent;
+	struct s_octree		*children[NUM_CHILDREN];
 	t_octree_content	*content;
+	t_bounds			bounds;
+	unsigned char		active_children;
 }				t_octree;
 
 
--- src/octree/octree.c
+++ src/octree/octree.c
@@ -6,7 +6,7 @@
 /*   By: wkorande <willehard@gmail.com>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/07/07 18:41:21 by wkorande          #+#    #+#             */
-/*   Updated: 2020/07/07 20:04:29 by wkorande         ###   ########.fr       */
+/*   Updated: 2020/07/09 14:53:55 by wkorande         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -33,7 +33,21 @@ t_octree	*octree_create_node(t_octree_content *content)
 		node->children[i] = NULL;
 		i++;
 	}
+	node->parent = NULL;
+	node->active_children = ACTIVE_NONE;
 	return (node);
+}
+
+void	octree_calc_bounds(t_octree *octree)
+{
+	if (!octree || !octree->parent)
+		return ;
+
+}
+
+void	octree_insert(t_octree *parent, t_octree *child, unsigned char octant)
+{
+	if (!parent || !child)
 }
 
 t_octree_content *octree_content_create(void *data, size_t data_size)
