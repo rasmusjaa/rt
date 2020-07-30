31d1077099239297a19d51aa0c1dc1fd82252c15
Sasu Luhtala <sluhtala@student.hive.fi>
2020-07-30 18:35:47 +0300

    working with plane uv rotation

--- src/scene/shape/plane.c
+++ src/scene/shape/plane.c
@@ -6,7 +6,7 @@
 /*   By: rjaakonm <rjaakonm@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/07/13 18:24:54 by wkorande          #+#    #+#             */
-/*   Updated: 2020/07/29 13:42:02 by rjaakonm         ###   ########.fr       */
+/*   Updated: 2020/07/30 16:37:29 by sluhtala         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,13 +24,21 @@ t_vec2 calc_hit_uv_plane(t_shape *plane, t_raycast_hit *hit)
 {
 	t_vec2		uv;
 	t_vec3	p;
-
+	t_vec3 	rrotate;
 	p = hit->point;
 	
+	rrotate = ft_invert_vec3(plane->rotation);
+
 	p = ft_sub_vec3(p, plane->position);
-	p = ft_rotate_vec3(hit->point, ft_invert_vec3(plane->rotation));
-	uv.x = (hit->point.x) / plane->material->u_scale;
-	uv.y = (hit->point.z) / plane->material->v_scale;
+	if (rrotate.z)
+	p = ft_rotate_vec3(hit->point, ft_make_vec3(0,0, rrotate.z));
+	if (rrotate.y)
+	p = ft_rotate_vec3(hit->point, ft_make_vec3(0,rrotate.y,0));
+	if (rrotate.x)
+	p = ft_rotate_vec3(hit->point, ft_make_vec3(rrotate.x, 0, 0));
+	
+	uv.x = (p.x) / plane->material->u_scale;
+	uv.y = (p.z) / plane->material->v_scale;
 	
 	uv.x = uv.x - floor(uv.x);
 	uv.y = uv.y - floor(uv.y);
