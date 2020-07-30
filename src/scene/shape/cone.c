0a963f029fe6c79e998fcf9c48fa8d6d1bdb8171
rasmusjaa <rasmus@lense.fi>
2020-07-30 16:33:07 +0300

    now backside of shapes is shown if shape is cut with bounds

--- src/scene/shape/cone.c
+++ src/scene/shape/cone.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   cone.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: wkorande <willehard@gmail.com>             +#+  +:+       +#+        */
+/*   By: rjaakonm <rjaakonm@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/07/13 18:27:12 by wkorande          #+#    #+#             */
-/*   Updated: 2020/07/28 21:54:13 by wkorande         ###   ########.fr       */
+/*   Updated: 2020/07/30 16:25:29 by rjaakonm         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -56,15 +56,17 @@ int	intersects_cone(t_ray *ray, t_shape *cone, t_raycast_hit *hit)
 		* ft_dot_vec3(ray->direction, v) * ft_dot_vec3(oc, v));
 	q.c = ft_dot_vec3(oc, v);
 	q.c = ft_dot_vec3(oc, oc) - (1 + theta_sqr) * q.c * q.c;
-	if (solve_quadratic(q, &hit->t, &hit->t2) && (hit->t > 0) &&
-		(hit->t < MAX_CLIP))
+	if (solve_quadratic(q, &hit->t, &hit->t2) && (hit->t > 0))
 	{
-		hit->point = point_on_ray(ray, hit->t);
-		if ((ft_dot_vec3(
-				ft_sub_vec3(hit->point, temp), v) < 0))
-			return (FALSE);
-		hit->distance = hit->t;
-		return (TRUE);
+		if (check_t_hits(ray, hit, cone))
+		{
+			hit->point = point_on_ray(ray, hit->t);
+			if ((ft_dot_vec3(
+					ft_sub_vec3(hit->point, temp), v) < 0))
+				return (FALSE);
+			hit->distance = hit->t;
+			return (TRUE);
+		}
 	}
 	return (FALSE);
 }
