ecb3961e7c8fb6da2f85c208400db32d5e721baa
Willehard Korander <willehard@gmail.com>
2020-07-31 19:20:20 +0300

    rollback cone intersect because it was broken. needs fix to get explode back

--- src/scene/shape/cone.c
+++ src/scene/shape/cone.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   cone.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: rjaakonm <rjaakonm@student.hive.fi>        +#+  +:+       +#+        */
+/*   By: wkorande <willehard@gmail.com>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/07/13 18:27:12 by wkorande          #+#    #+#             */
-/*   Updated: 2020/07/30 16:25:29 by rjaakonm         ###   ########.fr       */
+/*   Updated: 2020/07/31 19:18:29 by wkorande         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -33,21 +33,55 @@ t_vec2 calc_hit_uv_cone(t_shape *cone, t_raycast_hit *hit)
 	return (ft_make_vec2(0,0));
 }
 
+// int	intersects_cone(t_ray *ray, t_shape *cone, t_raycast_hit *hit)
+// {
+// 	t_quadratic	q;
+// 	t_vec3		oc;			// cone position to ray origin
+// 	t_vec3		v;			// cone direction
+// 	double		theta_sqr;
+// 	t_vec3	temp;
+
+// 	temp = cone->material->explode > EPSILON ? ft_add_vec3(cone->position, ft_mul_vec3(cone->target, cone->material->explode * ft_inv_lerp_d((double)rand(), 0, RAND_MAX)))  : cone->target;
+
+
+// 	theta_sqr = ft_deg_to_rad(cone->angle) * ft_deg_to_rad(cone->angle);
+
+// 	v = cone->target;
+// 	oc = ft_sub_vec3(ray->origin, temp);
+// 	q.a = ft_dot_vec3(ray->direction, v);
+// 	q.a = ft_dot_vec3(ray->direction, ray->direction) -
+// 		(1 + theta_sqr) * q.a * q.a;
+// 	q.b = 2.0 * (ft_dot_vec3(ray->direction, oc) -
+// 		(1 + theta_sqr)
+// 		* ft_dot_vec3(ray->direction, v) * ft_dot_vec3(oc, v));
+// 	q.c = ft_dot_vec3(oc, v);
+// 	q.c = ft_dot_vec3(oc, oc) - (1 + theta_sqr) * q.c * q.c;
+// 	if (solve_quadratic(q, &hit->t, &hit->t2) && (hit->t > 0))
+// 	{
+// 		if (check_t_hits(ray, hit, cone))
+// 		{
+// 			hit->point = point_on_ray(ray, hit->t);
+// 			if ((ft_dot_vec3(
+// 					ft_sub_vec3(hit->point, temp), v) > 0))
+// 				return (FALSE);
+// 			hit->distance = hit->t;
+// 			return (TRUE);
+// 		}
+// 	}
+// 	return (FALSE);
+// }
+
 int	intersects_cone(t_ray *ray, t_shape *cone, t_raycast_hit *hit)
 {
 	t_quadratic	q;
 	t_vec3		oc;			// cone position to ray origin
 	t_vec3		v;			// cone direction
 	double		theta_sqr;
-	t_vec3	temp;
-
-	temp = cone->material->explode > EPSILON ? ft_add_vec3(cone->position, ft_mul_vec3(cone->target, cone->material->explode * ft_inv_lerp_d((double)rand(), 0, RAND_MAX)))  : cone->target;
-
 
 	theta_sqr = ft_deg_to_rad(cone->angle) * ft_deg_to_rad(cone->angle);
 
 	v = cone->target;
-	oc = ft_sub_vec3(ray->origin, temp);
+	oc = ft_sub_vec3(ray->origin, cone->position);
 	q.a = ft_dot_vec3(ray->direction, v);
 	q.a = ft_dot_vec3(ray->direction, ray->direction) -
 		(1 + theta_sqr) * q.a * q.a;
@@ -56,17 +90,15 @@ int	intersects_cone(t_ray *ray, t_shape *cone, t_raycast_hit *hit)
 		* ft_dot_vec3(ray->direction, v) * ft_dot_vec3(oc, v));
 	q.c = ft_dot_vec3(oc, v);
 	q.c = ft_dot_vec3(oc, oc) - (1 + theta_sqr) * q.c * q.c;
-	if (solve_quadratic(q, &hit->t, &hit->t2) && (hit->t > 0))
+	if (solve_quadratic(q, &hit->t, &hit->t2) && (hit->t > 0) &&
+		(hit->t < MAX_CLIP))
 	{
-		if (check_t_hits(ray, hit, cone))
-		{
-			hit->point = point_on_ray(ray, hit->t);
-			if ((ft_dot_vec3(
-					ft_sub_vec3(hit->point, temp), v) < 0))
-				return (FALSE);
-			hit->distance = hit->t;
-			return (TRUE);
-		}
+		hit->point = point_on_ray(ray, hit->t);
+		if ((ft_dot_vec3(
+				ft_sub_vec3(hit->point, cone->position), v) > 0))
+			return (FALSE);
+		hit->distance = hit->t;
+		return (TRUE);
 	}
 	return (FALSE);
 }
