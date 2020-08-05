51e3451f396db63930f0040be1d121a60f03b59b
Sasu Luhtala <sluhtala@student.hive.fi>
2020-08-05 15:41:28 +0300

    shade.c norme r is refraction

--- src/render/shade.c
+++ src/render/shade.c
@@ -25,14 +25,15 @@ static t_rgba	color_from_lights(t_scene *scene, t_raycast_hit *hit)
 
 	i = 0;
 	total_light = ft_make_rgba(0, 0, 0, 1);
-	while ( i < scene->num_lights)
+	while (i < scene->num_lights)
 	{
 		s = 0;
 		light = scene->lights[i].color;
 		if (scene->scene_config.shadows)
 			s = calc_shadow(scene->lights[i], *hit, scene);
 		if (s < 1 && scene->scene_config.shading)
-			light = ft_mul_rgba(light, calc_diffuse(scene->lights[i], *hit, scene));
+			light = ft_mul_rgba(light, calc_diffuse(scene->lights[i],
+			*hit, scene));
 		light = ft_mul_rgba(light, 1.0 - s);
 		total_light = ft_add_rgba(total_light, light);
 		i++;
@@ -40,41 +41,58 @@ static t_rgba	color_from_lights(t_scene *scene, t_raycast_hit *hit)
 	return (ft_clamp_rgba(total_light));
 }
 
-
-static t_rgba	color_from_shape(t_rgba color, t_scene *scene, t_raycast_hit *hit)
+static int		set_rec_calced(t_scene *scene, t_raycast_hit *hit,
+	t_rgba *rec, t_rgba *rac)
 {
-	t_rgba		rec;
-	t_rgba		rac;
-	double		refraction;
-	double		fresnel;
+	double fresnel;
+	double refraction;
+
+	refraction = hit->shape->material->refra_index;
+	fresnel = calc_fresnel(hit->normal, hit->ray.direction, refraction);
+	*rec = calc_reflect(scene, *hit, hit->ray.direction,
+		hit->normal, hit->depth);
+	*rac = ft_lerp_rgba(*rac, *rec, fresnel);
+	return (TRUE);
+}
+
+static t_rgba	color_diffuse_texture(t_raycast_hit *hit, t_rgba color)
+{
+	return (ft_mul_rgba_rgba(color,
+		ft_add_rgba(hit->shape->material->diffuse,
+			sample_texture(hit->shape->material->texture, hit->uv))));
+}
+
+static t_rgba	color_from_shape(t_rgba color, t_scene *scene,
+	t_raycast_hit *hit)
+{
+	t_rgba		rera[2];
+	double		r;
 	int			rec_calced;
 
-//	color = ft_mul_rgba_rgba(color, hit->shape->material->diffuse); //shape->color to material->diffuse /ilman tata mustavalkoseks
-	color = ft_mul_rgba_rgba(color, ft_add_rgba(hit->shape->material->diffuse, sample_texture(hit->shape->material->texture, hit->uv)));
+	color = color_diffuse_texture(hit, color);
 	rec_calced = FALSE;
-	if (scene->scene_config.opacity && hit->shape->material->opacity < 1 - EPSILON) //material->opacity
+	if (scene->scene_config.opacity && hit->shape->material->opacity <
+		1 - EPSILON)
 	{
-		refraction = scene->scene_config.refraction ? hit->shape->material->refra_index : 1; //material->refra_index
-		rac = calc_refract(scene, hit->ray.direction, *hit, refraction, hit->depth);
-		if (scene->scene_config.refraction && refraction > 1 + EPSILON)
-		{
-			fresnel = calc_fresnel(hit->normal, hit->ray.direction, refraction);
-			rec = calc_reflect(scene, *hit, hit->ray.direction, hit->normal, hit->depth);
-			rec_calced = TRUE;
-			rac = ft_lerp_rgba(rac, rec, fresnel);
-		}
-		color = ft_lerp_rgba(color, rac, 1 - hit->shape->material->opacity); //opacity to material->opacity
+		r = scene->scene_config.refraction ?
+			hit->shape->material->refra_index : 1;
+		rera[1] = calc_refract(scene, hit->ray.direction, *hit, r, hit->depth);
+		if (scene->scene_config.refraction && r > 1 + EPSILON)
+			rec_calced = set_rec_calced(scene, hit, &rera[0], &rera[1]);
+		color = ft_lerp_rgba(color, rera[1], 1 - hit->shape->material->opacity);
 	}
-	if (scene->scene_config.reflection && hit->shape->material->reflection > EPSILON) //material
+	if (scene->scene_config.reflection &&
+		hit->shape->material->reflection > EPSILON)
 	{
 		if (!rec_calced)
-			rec = calc_reflect(scene, *hit, hit->ray.direction, hit->normal, hit->depth);
-		color = ft_lerp_rgba(color, rec, hit->shape->material->reflection); //material
+			rera[0] = calc_reflect(scene, *hit, hit->ray.direction,
+				hit->normal, hit->depth);
+		color = ft_lerp_rgba(color, rera[0], hit->shape->material->reflection);
 	}
 	return (ft_clamp_rgba(color));
 }
 
-t_rgba	shade(t_scene *scene, t_raycast_hit *hit)
+t_rgba			shade(t_scene *scene, t_raycast_hit *hit)
 {
 	t_rgba		ambient;
 	t_rgba		color;
@@ -85,7 +103,9 @@ t_rgba	shade(t_scene *scene, t_raycast_hit *hit)
 	color = color_from_lights(scene, hit);
 	color = ft_add_rgba(color, scene->scene_config.ambient);
 	color = color_from_shape(color, scene, hit);
-	if (scene->scene_config.specular && hit->shape->material->shininess > EPSILON) //material->shininess
-		color = ft_add_rgba(color, calc_specular(scene, *hit, scene->cameras[scene->cur_camera]));
+	if (scene->scene_config.specular &&
+		hit->shape->material->shininess > EPSILON)
+		color = ft_add_rgba(color,
+			calc_specular(scene, *hit, scene->cameras[scene->cur_camera]));
 	return (ft_clamp_rgba(color));
 }
